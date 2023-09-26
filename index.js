35e6f10258af8854cb75a05c010f3d7d0bc48716
Skylar-Lorena <lorayne911@gmail.com>
2023-09-26 22:24:13 +0300

    steps guide to demo how to use event listeners.

---
+++ index.js
@@ -0,0 +1,25 @@
+// Get a reference to the task input element.
+
+// Get a reference to the add task button element.
+
+// Get a reference to the task list element.
+
+// Add an event listener to the add task button element.
+
+// Get the value of the task input element.
+
+// Create a new task list item element.
+
+// Add the task text to the task list item element.
+
+// Add the task list item element to the task list element.
+
+// Clear the task input element.
+
+// Add an event listener to the task list element.
+
+// Get the target element of the event.
+
+// If the target element is a task list item element, toggle its completed class.
+
+
